8c6e85568e81ee43d4ea57f50e3cd9dd866e6692
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-07-04 11:22:44 +0530

    eslint configs added

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Layout from '../components/Layout'
 import Container from '../components/Layout/Container'
 
 const Home = () => {
+  const user = null
   return (
     <Layout title="Oviz Fashions | Your Charming Beauty is Our Passion">
       <Container>
