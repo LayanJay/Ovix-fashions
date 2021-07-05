880f58834ca01d323502923fc098e375935b0d62
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-07-05 13:45:58 +0530

    WIP: homepage main categories section

---
+++ modules/homepage/MainCategories.jsx
@@ -0,0 +1,11 @@
+import Container from '../../components/Layout/Container'
+
+const MainCategories = () => {
+  return (
+    <>
+      <Container>MainCategories</Container>
+    </>
+  )
+}
+
+export default MainCategories
