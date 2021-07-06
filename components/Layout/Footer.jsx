0275d6317471ae181afadceda6119f48722b2400
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-07-06 14:38:03 +0530

    add: minor footer styles

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -3,72 +3,71 @@ import { MdLocalPhone } from 'react-icons/md'
 import { MdMailOutline } from 'react-icons/md'
 import { AiOutlineInstagram } from 'react-icons/ai'
 import { FaFacebookF } from 'react-icons/fa'
+import Container from '../Layout/Container'
 
 const Footer = () => {
   return (
-    <footer className="footer bg-brown-dark opacity-75 relative pt-1 border-b-2 border-blue-700">
-      <div className="container mx-auto px-6">
-        <div className="sm:flex sm:mt-8">
-          <div className="mt-8 sm:mt-0 sm:w-full md:px-15 lg:px-24 flex flex-col md:flex-row justify-between">
-            <div className="flex flex-col">
-              <div className="my-2 flex flex-row">
-                <div className=" bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
-                  <MdLocationOn />
-                </div>
-                <div className="text-white pl-4 w-56 flex items-center">
-                  No. 420, Vihara Mawatha, Kaduwela, Malabe
-                </div>
+    <footer className="bg-brown-footer bg-opacity-75 relative border-b-2 border-blue-700">
+      <Container>
+        <section className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 gap-8 place-items-center py-10">
+          <div className="md:col-span-1 flex flex-col justify-center font-roboto">
+            <div className="inline-flex items-center my-3 space-x-4 group cursor-default">
+              <div className="bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
+                <MdLocationOn />
               </div>
-
-              <div className="my-2 flex flex-row">
-                <div className=" bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
-                  <MdLocalPhone />
-                </div>
-                <div className="text-white pl-4 w-56 flex items-center">
-                  +94 76 6868 231
-                </div>
-              </div>
-
-              <div className="my-2 flex flex-row">
-                <div className=" bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
-                  <MdMailOutline />
-                </div>
-                <div className="text-white pl-4 w-56 flex items-center">
-                  info@ovizfashions.lk
-                </div>
+              <div className="text-white w-56 group-hover:underline">
+                No. 420, Vihara Mawatha, Kaduwela, Malabe
               </div>
             </div>
 
-            <div className="flex flex-col w-full sm:w-96 lg:mr-20">
-              <span className="font-bold text-white mt-4 md:mt-0 mb-2 font-playFair text-4xl">
-                About us
-              </span>
-              <span className="my-2">
-                <div className="text-white">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                </div>
-              </span>
-              <span className="my-2 flex gap-4">
-                <div className=" bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
+            <div className="inline-flex items-center my-3 space-x-4 group cursor-default">
+              <div className=" bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
+                <MdLocalPhone />
+              </div>
+              <div className="text-white w-56 group-hover:underline">
+                +94 76 6868 231
+              </div>
+            </div>
+
+            <div className="inline-flex items-center my-3 space-x-4 group cursor-default">
+              <div className=" bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
+                <MdMailOutline />
+              </div>
+              <div className="text-white w-56 group-hover:underline">
+                info@ovizfashions.lk
+              </div>
+            </div>
+          </div>
+
+          <div className="md:col-span-1 flex flex-col justify-center select-none">
+            <span className="font-bold text-white font-playFair text-4xl mb-4">
+              About us
+            </span>
+
+            <div className="text-white font-roboto max-w-md mb-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore
+            </div>
+
+            <div className="inline-flex space-x-4">
+              <a target="_blank" rel="me" href="#">
+                <div className=" bg-brown-light text-brown-dark hover:text-offWhite hover:bg-brown-footer transition ease-in cursor-pointer w-10 py-4 text-2xl flex justify-center items-center rounded-full">
                   <AiOutlineInstagram />
                 </div>
-                <div className=" bg-brown-light text-brown-dark w-10 py-4 text-2xl flex justify-center items-center rounded-full">
+              </a>
+              <a target="_blank" rel="me" href="#">
+                <div className=" bg-brown-light text-brown-dark hover:text-offWhite hover:bg-brown-footer transition ease-in cursor-pointer w-10 py-4 text-2xl flex justify-center items-center rounded-full">
                   <FaFacebookF />
                 </div>
-              </span>
+              </a>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="container mx-auto px-6">
-        <div className="mt-16 flex flex-col items-center">
-          <div className="sm:w-2/3 text-center py-6">
-            <p className="text-sm mb-2 text-white">
-              © 2021 Oviz Fashions. All rights reserved
-            </p>
-          </div>
-        </div>
+        </section>
+      </Container>
+      <div className="bg-brown-footer bg-opacity-70 font-roboto text-xs py-5">
+        <p className="text-center text-white">
+          © 2021 Oviz Fashions. All rights reserved
+        </p>
       </div>
     </footer>
   )
