3f37c2b951e8bf6bb8e221a43cdb8eb8f370403e
Menake Dasunpriya <menakedasunpriya@gmail.com>
2021-07-07 17:00:33 +0530

    code improvements

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,16 +1,9 @@
-import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import SmallFooter from './SmallFooter'
 
 const Layout = ({ children, title, inverted, fullFooter }) => {
-  const [isFullFooter, setIsFullFooter] = useState(true)
-
-  useEffect(() => {
-    setIsFullFooter(fullFooter)
-  }, [fullFooter])
-
   return (
     <>
       <Head>
@@ -19,7 +12,7 @@ const Layout = ({ children, title, inverted, fullFooter }) => {
       <main className="text-textBlack font-roboto relative min-h-screen">
         <Navbar inverted={inverted} />
         {children}
-        {isFullFooter ? <Footer /> : <SmallFooter />}
+        {fullFooter ? <Footer /> : <SmallFooter />}
       </main>
     </>
   )
