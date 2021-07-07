41fd6867af464f47e4f4db2006636c1a61bc1739
Navindu <navanavi3@gmail.com>
2021-07-07 19:41:05 +0530

    add: animate navbar on scroll

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -4,16 +4,50 @@ import ovizWhite from '../../public/assets/oviz-o-white.png'
 import ovizBlack from '../../public/assets/oviz-o-black.png'
 import bagBlack from '../../public/assets/totebag-black.svg'
 import bagWhite from '../../public/assets/totebag-white.svg'
+import { useEffect, useState } from 'react'
+import gsap from 'gsap'
 
 const Navbar = ({ inverted }) => {
+  let animated = false
+
+  const [inv, setInverted] = useState(inverted)
+
+  useEffect(() => {
+    window.onscroll = () => {
+      if (window.pageYOffset > 100 && !animated) {
+        gsap.to('nav', {
+          height: 80,
+          backgroundColor: '#fff',
+          duration: 0.5,
+          ease: 'expo.inOut',
+        })
+        setInverted(false)
+
+        animated = true
+      } else if (window.pageYOffset < 100 && animated) {
+        gsap.to('nav', {
+          height: 112,
+          backgroundColor: 'transparent',
+          ease: 'expo.inOut',
+          duration: 0.5,
+        })
+        animated = false
+        setInverted(true)
+      }
+    }
+  }, [])
+
   return (
-    <nav className="font-roboto flex flex-row h-28 text-left fixed justify-between py-4 px-6 items-center w-full sm:px-16">
+    <nav
+      id="nav"
+      className="z-50 font-roboto flex flex-row h-28 text-left fixed justify-between py-4 px-6 items-center w-full sm:px-16"
+    >
       <div className=" ">
         <Link href="/">
           <a>
             <div className="w-8">
               <Image
-                src={inverted ? ovizWhite : ovizBlack}
+                src={inv ? ovizWhite : ovizBlack}
                 alt="navbar elements"
                 layout="intrinsic"
               />
@@ -27,7 +61,7 @@ const Navbar = ({ inverted }) => {
             <div className="">
               <div className="w-9">
                 <Image
-                  src={inverted ? bagWhite : bagBlack}
+                  src={inv ? bagWhite : bagBlack}
                   alt="navbar elements"
                   layout="intrinsic"
                 />
