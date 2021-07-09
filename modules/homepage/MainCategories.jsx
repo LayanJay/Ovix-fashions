e90c45c638353e87d6665928c1e05482d8e15da7
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-07-09 21:19:59 +0530

    add: main categories section animations added

--- modules/homepage/MainCategories.jsx
+++ modules/homepage/MainCategories.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 import Image from 'next/image'
 import Link from 'next/link'
 import Container from '../../components/Layout/Container'
@@ -5,10 +8,65 @@ import shopMens from '../../public/samples/shop_men.png'
 import shopWomens from '../../public/samples/shop_women.png'
 
 const MainCategories = () => {
+  gsap.registerPlugin(ScrollTrigger)
+
+  useEffect(() => {
+    const tl1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#men',
+        start: 'top 65%',
+        markers: true,
+      },
+    })
+
+    const tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#women',
+        start: 'top 65%',
+        markers: true,
+      },
+    })
+
+    tl1
+      .from('#men', {
+        delay: 0.3,
+        duration: 0.8,
+        ease: 'power2.out',
+        opacity: 0,
+      })
+      .from(
+        '#menText',
+        {
+          duration: 1,
+          y: 50,
+          opacity: 0,
+          ease: 'power3.out',
+        },
+        0.6
+      )
+
+    tl2
+      .from('#women', {
+        delay: 0.3,
+        duration: 0.8,
+        ease: 'power2.out',
+        opacity: 0,
+      })
+      .from(
+        '#womenText',
+        {
+          duration: 1,
+          y: 50,
+          opacity: 0,
+          ease: 'power3.out',
+        },
+        0.6
+      )
+  }, [])
   return (
     <Container>
       <section className="grid grid-rows-1 grid-cols-1 sm:grid-cols-2 gap-8 py-10">
-        <div className="relative sm:col-span-1 group">
+        <div id="men" className="relative sm:col-span-1 group">
           <Link href="/men">
             <a>
               <div className="sm:max-w-xl sm:w-auto w-72 mx-auto">
@@ -20,7 +78,10 @@ const MainCategories = () => {
                   quality={80}
                 />
               </div>
-              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+              <div
+                id="menText"
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center"
+              >
                 <h2 className="font-playFair font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl leading-normal tracking-wide mb-6">
                   Shop Men&apos;s
                 </h2>
@@ -32,9 +93,9 @@ const MainCategories = () => {
           </Link>
         </div>
 
-        <Link href="/women">
-          <a>
-            <div className="relative sm:col-span-1 group">
+        <div id="women" className="relative sm:col-span-1 group">
+          <Link href="/women">
+            <a>
               <div className="sm:max-w-xl sm:w-auto w-72 mx-auto">
                 <Image
                   className="group-hover:opacity-90 transition ease-in"
@@ -44,7 +105,10 @@ const MainCategories = () => {
                   quality={80}
                 />
               </div>
-              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+              <div
+                id="womenText"
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center"
+              >
                 <h2 className="font-playFair font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl leading-normal tracking-wide mb-6">
                   Shop Women&apos;s
                 </h2>
@@ -52,9 +116,9 @@ const MainCategories = () => {
                   Discover Now
                 </p>
               </div>
-            </div>
-          </a>
-        </Link>
+            </a>
+          </Link>
+        </div>
       </section>
     </Container>
   )
