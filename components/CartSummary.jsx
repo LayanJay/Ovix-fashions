6dc35166751947d4a447a78c57e9d51c5a935751
Navindu <navanavi3@gmail.com>
2021-07-11 21:38:29 +0530

    feat: load bag into cart summary

--- components/CartSummary.jsx
+++ components/CartSummary.jsx
@@ -1,18 +1,27 @@
 import InputBox from './InputBox'
 import Button from './Button'
+import { useCartState } from '../context/cart'
 
 const CartSummary = () => {
+  const { line_items, subtotal } = useCartState()
+  let discount = 0 // TODO
+
   return (
     <div className="w-full flex-1 my-8">
       <div className="w-full border-2 border-brown-dark p-6 rounded-lg">
-        <div className="grid grid-cols-2 items-center justify-center">
-          <div className="font-playFair font-bold text-lg md:text-xl text-brown-dark my-3">
-            Emerald Corporate Micro Check Regular Fit
+        {line_items.map((item) => (
+          <div
+            className="grid grid-cols-2 items-center justify-center"
+            key={item.id}
+          >
+            <div className="font-playFair font-bold text-lg md:text-xl text-brown-dark my-3">
+              {item.name}
+            </div>
+            <div className="text-right text-lg font-bold text-brown-dark">
+              {item.line_total.formatted_with_code}
+            </div>
           </div>
-          <div className="text-right text-lg font-bold text-brown-dark">
-            2000 LKR
-          </div>
-        </div>
+        ))}
 
         <hr className="border-none bg-brown-dark h-0.5 my-3" />
 
@@ -29,7 +38,7 @@ const CartSummary = () => {
         <div className="grid grid-cols-2 items-center justify-center mt-4">
           <div className="font-bold text-lg text-brown-dark">Subtotal</div>
           <div className="text-right text-lg font-bold text-brown-dark">
-            2000 LKR
+            {subtotal?.formatted_with_code}
           </div>
         </div>
 
@@ -43,7 +52,7 @@ const CartSummary = () => {
         <div className="grid grid-cols-2 items-center justify-center mt-8">
           <div className="font-bold text-2xl text-brown-dark">Total</div>
           <div className="text-right text-2xl font-bold text-brown-dark">
-            LKR 5222
+            {subtotal?.formatted_with_code} LKR
           </div>
         </div>
       </div>
